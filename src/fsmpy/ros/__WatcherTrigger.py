c5f782261a0eb1c340408261f67bd8cd654ce0b5
James Stewart <james.stewy@gmail.com>
2017-08-02 19:56:34 +1000

    indent issues

--- src/fsmpy/ros/__WatcherTrigger.py
+++ src/fsmpy/ros/__WatcherTrigger.py
@@ -11,35 +11,35 @@ class WatcherTrigger(Watcher):
 	name:		  the name of the Watcher used in logging
 	service_name: the name of the service to be used
 	'''
-    def __init__(self, name, service_name):
-        super(WatcherTrigger, self).__init__(name)
+	def __init__(self, name, service_name):
+		super(WatcherTrigger, self).__init__(name)
 
-        if service_name == None or service_name == "":
-            self.logfatal("service_name not set")
-        self.__service_name = service_name
+		if service_name == None or service_name == "":
+		    self.logfatal("service_name not set")
+		self.__service_name = service_name
 
-        self.__service = None
+		self.__service = None
 
     ### Method Overrides ###
 
-    def start(self):
-        self.__service = Service(self.__service_name, Trigger, self.handler)
-        self.logdebug("started")
+	def start(self):
+	    self.__service = Service(self.__service_name, Trigger, self.handler)
+	    self.logdebug("started")
 
-    def stop(self):
-        self.__service.shutdown()
-    	self.logdebug("stopped")
+	def stop(self):
+		self.__service.shutdown()
+		self.logdebug("stopped")
 
-    ###  / Method Overrides ###
-    ### Overridable methods ###
+	###  / Method Overrides ###
+	### Overridable methods ###
 
 	'''
 	Handles incoming service requests.
 	By default, the watcher is triggered and True is returned.
 	'''
-    def handler(self, req):
-        with self.event.get_lock():
-            self.event.value = True
-        return TriggerResponse(True, "")
+	def handler(self, req):
+	    with self.event.get_lock():
+	        self.event.value = True
+	    return TriggerResponse(True, "")
 
-    ### / Overridable methods ###
+	### / Overridable methods ###
