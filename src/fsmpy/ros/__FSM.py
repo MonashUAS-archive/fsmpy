28324776e05eb5c6301033b31c37545d3be8a4aa
James Stewart <james.stewy@gmail.com>
2017-10-11 15:11:33 +1100

    Publish state string to topic on transition

--- src/fsmpy/ros/__FSM.py
+++ src/fsmpy/ros/__FSM.py
@@ -1,5 +1,6 @@
 from .. import FSM as FSMBase
 from .. import set_logger
+from std_msgs.msg import String
 import rospy
 
 '''
@@ -10,22 +11,25 @@ ros.FSM initialises a ROS node and sets the fsmpy logger to rosout.
 class FSM(FSMBase):
 	'''
 	name:			the name of the FSM used in logging
+	topic:			the topic to publish the state string too
 	save_file=None:	path to a file to be used for storing the current state stack
 
 	All other arguments are passed through to the call to rospy.init_node.
 	'''
-	def __init__(self, name, *args, **kw):
+	def __init__(self, name, topic, *args, **kw):
 		if 'save_file' in kw:
 			save_file = kw.pop('save_file')
 		else:
 			save_file = None
 		rospy.init_node(*args, **kw)
 		set_logger("rosout")
+		self.__pub = rospy.Publisher(topic, String, queue_size=2, latch=True)
 		super(FSM, self).__init__(name, save_file=save_file)
 
 	### Method Overrides ###
 
 	def on_transition(self, next_state):
+		self.__pub.publish(next_state.name)
 		self.loginfo("Transitioned to State '%s'"%(next_state.name))
 
 	def delay(self):
